59344ac6d341491e587729bdf6d0365bb59363e7
Florent Gallaire <fgallaire@gmail.com>
2017-09-20 05:44:32 +0200

    http://obspm static export

---
+++ wp-atom.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=http://obspm.flext.net/feed/atom/">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "http://obspm.flext.net/feed/atom/";
+		</script>
+
+		<p>You are being redirected to <a href="http://obspm.flext.net/feed/atom/">http://obspm.flext.net/feed/atom/</a></p>
+	</body>
+</html>
